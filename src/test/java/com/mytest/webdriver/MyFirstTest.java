6fc206e8ada971d56e4db212e84a8cf30604425f
kroy <kallol.roy@sentrana.com>
2015-09-21 16:24:16 +0600

    init commit

---
+++ src/test/java/com/mytest/webdriver/MyFirstTest.java
@@ -0,0 +1,16 @@
+package com.mytest.webdriver;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MyFirstTest {
+    @Test
+    public void startDriver(){
+        WebDriver driver = new FirefoxDriver();
+        driver.navigate().to("https://biq-dev.sentrana.com/biq");
+        Assert.assertTrue("title should start with MarketMover",driver.getTitle().startsWith("arketMover"));
+        driver.close();
+        driver.quit();
+    }
+}
